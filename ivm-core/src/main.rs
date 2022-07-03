36e4cabba9b9ec53b58b040a13fe28c22932ba37
ImajinDevon <itsimaginedev@gmail.com>
2022-07-02 21:40:28 -0400

    Changes

--- ivm-core/src/main.rs
+++ ivm-core/src/main.rs
@@ -3,8 +3,8 @@ use ivm_common::cli::LinearLogger;
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
-    let process = LinearLogger::new("ivm", 0);
-    process.log(format!("ivm version {VERSION}"));
+    let logger = LinearLogger::new("ivm", 0);
+    logger.log(format!("ivm version {VERSION}"));
 
     // TODO add proper cli functionality
 }
